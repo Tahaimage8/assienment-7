9ad1823116538f4dcc41d78c79f4efef65a560e7
tahaimage8 <tahaimage8@gmail.com>
2026-04-14 22:52:01 +0600

    complate the body api section without funcinality

--- src/Compornent/StudentDetails.jsx
+++ src/Compornent/StudentDetails.jsx
@@ -1,4 +1,5 @@
 import { use } from "react";
+import Card from "./Card";
 
 const getFriends = async () => {
   const res = await fetch("http://localhost:3000/friends.json");
@@ -8,37 +9,46 @@ const getFriends = async () => {
 const StudentDetails = () => {
   const friends = use(getFriends());
 
-  console.log(friends)
+  console.log(friends);
   return (
-    <div className="mb-10 lg:border-b lg:border-[#E9E9E9] md:border-b md:border-[#E9E9E9]">
-      <div className="gap-6 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mt-10 mb-10">
-
-        <div className="shadow-2xl p-10 text-center">
-          <div>{friends.length}</div>
-          <div>Total Friends</div>
-        </div>
-
-        <div className="shadow-2xl p-10 text-center">
-          <div>{friends.filter(f => f.status === "on-track").length}</div>
-          <div>On Track</div>
-        </div>
-
-        <div className="shadow-2xl p-10 text-center">
-          <div>{friends.filter(f => f.status === "overdue").length}</div>
-          <div>Need Attention</div>
-        </div>
-
-        <div className="shadow-2xl p-10 text-center">
-          <div>
-            {friends.reduce((total, f) => total + f.days_since_contact, 0)}
+    <div>
+      <div className="mb-10 lg:border-b lg:border-[#E9E9E9] md:border-b md:border-[#E9E9E9]">
+        <div className="gap-6 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mt-10 mb-10">
+          <div className="shadow-2xl p-10 text-center">
+            <div>{friends.length}</div>
+            <div>Total Friends</div>
           </div>
-          <div>Interactions This Month</div>
+
+          <div className="shadow-2xl p-10 text-center">
+            <div>{friends.filter((f) => f.status === "on-track").length}</div>
+            <div>On Track</div>
+          </div>
+
+          <div className="shadow-2xl p-10 text-center">
+            <div>{friends.filter((f) => f.status === "overdue").length}</div>
+            <div>Need Attention</div>
+          </div>
+
+          <div className="shadow-2xl p-10 text-center">
+            <div>
+              {friends.reduce((total, f) => total + f.days_since_contact, 0)}
+            </div>
+            <div>Interactions This Month</div>
+          </div>
         </div>
-
       </div>
+
+      {/* //---- ------------------------------------*/}
+
+    <div>
+        <h2 className="text-[#1F2937] text-[24px] font-semibold">Your Friends</h2> <br />
+            <div className="grid grid-cols-4 gap-10 ">
+      {
+        friends.map((friend) => <Card key={friend.id} friend={friend} />)
+      }
     </div>
-
-
+    </div>
+    </div>
   );
 };
 
